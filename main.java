fc47a0b030d9c7ba9983074f1c84e18078355fb1
Fusa Vladislav <fusavlad@gmail.com>
2023-08-13 02:03:15 +0300

    Pizza Bill Generator 0.2: - adding new class DeluxPizza witch extends class pizza and limits the user access to the possibility in adding extra toppings and extra chess to the delux pizza but still gives possibility to take Away

--- main.java
+++ main.java
@@ -1,8 +1,15 @@
 public class main {
     public static void main(String[] args) {
-        Pizza basePizza = new Pizza(true);
-        basePizza.addExtraTopping();
-        basePizza.takeAway();
-        basePizza.getBill();
+//        Pizza basePizza = new Pizza(true);
+//        basePizza.addExtraTopping();
+//        basePizza.takeAway();
+//        basePizza.getBill();
+
+        DeluxPizza deluxPizza = new DeluxPizza(false);
+        deluxPizza.addExtraTopping();
+        deluxPizza.addExtraCheese();
+        deluxPizza.takeAway();
+        deluxPizza.getBill();
+
     }
 }
